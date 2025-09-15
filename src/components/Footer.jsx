8f809df8734f2ec33c6a950003672463adde888e
khushi gupta <kg834208@gmail.com>
2025-09-16 02:28:16 +0530

    done foot

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-neutral-900 flex items-center mt-20 flex-col text-gray-300'>
+        <h1 className='mt-2'>© 2025 Khushi Gupta. Made with ❤️ and lots of love</h1>
+        <p className='text-sm m-1'>Crafting digital experiences that inspire and engage. Always learning, always building something new.</p>
+    </div>
+  )
+}
+
+export default Footer
